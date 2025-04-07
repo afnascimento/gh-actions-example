1c28148bbc190677cf599bcdf2a64b8f9a5017cd
Afonso NASCIMENTO <afnascimento@pictet-technologies.com>
2025-04-07 10:48:47 +0200

    Add java app

---
+++ java-app/src/test/java/com/example/javaapp/JavaAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test will check if the Spring application context loads successfully
+	}
+
+}
